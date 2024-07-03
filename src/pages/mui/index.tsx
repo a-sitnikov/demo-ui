61fb301aa6482234f9c07512cc740b0b0d05bfe6
Anatoliy Sitnikov <a.sitnikov@outlook.com>
2024-07-03 23:07:11 +0300

    mui theme

--- src/pages/mui/index.tsx
+++ src/pages/mui/index.tsx
@@ -1,15 +1,34 @@
-import { Container, ThemeProvider, createTheme } from "@mui/material";
+import {
+  Container,
+  FormControlLabel,
+  Switch,
+  ThemeProvider,
+  createTheme,
+} from "@mui/material";
 import { AppWrap } from "./ui";
 import { PageHeader } from "./view/page-header";
 import { ItemsTable } from "./view/table";
+import { useState } from "react";
 
-const theme = createTheme({
+const customizedTheme = createTheme({
   typography: {
     fontFamily: "Yandex Sans Text",
   },
 });
 
+const defaultTheme = createTheme({});
+
 export const MUIPage = () => {
+  const [theme, setTheme] = useState(defaultTheme);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      setTheme(customizedTheme);
+    } else {
+      setTheme(defaultTheme);
+    }
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <AppWrap>
@@ -18,6 +37,12 @@ export const MUIPage = () => {
           <ItemsTable />
         </Container>
       </AppWrap>
+      <FormControlLabel
+        control={
+          <Switch checked={theme === customizedTheme} onChange={handleChange} />
+        }
+        label="Customized theme"
+      />
     </ThemeProvider>
   );
 };
